8c2905970bbbd731b52eee1b333755f4781de70f
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-03-11 19:03:25 +0200

    fix routing to category and product endpoints

--- api-gateway/src/main/java/com/ecommerce/apigateway/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/ecommerce/apigateway/configuration/ApiGatewayConfiguration.java
@@ -3,13 +3,14 @@ package com.ecommerce.apigateway.configuration;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@org.springframework.context.annotation.Configuration
-public class Configuration {
+@Configuration
+public class ApiGatewayConfiguration {
     @Bean
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder) throws Exception {
-        return builder.routes().route(
-                predicateSpec -> predicateSpec.path("/category/**").uri("lb://category")
-        ).build();
+        return builder.routes()
+                .route(p-> p.path("/api/v1/category/**").uri("lb://product-service"))
+                .route(p-> p.path("/api/v1/product/**").uri("lb://product-service")).build();
     }
 }
