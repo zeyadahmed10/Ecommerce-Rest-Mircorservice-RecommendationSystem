dcf2c71579c1f2fdb69b18d29e92b31054469eb6
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-03-11 18:39:43 +0200

    removing naming server from register

--- naming-server/src/main/java/com/ecommerce/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/ecommerce/namingserver/NamingServerApplication.java
@@ -3,9 +3,9 @@ package com.ecommerce.namingserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 
 @SpringBootApplication
-@EnableEurekaServer
 public class NamingServerApplication {
 
 	public static void main(String[] args) {
