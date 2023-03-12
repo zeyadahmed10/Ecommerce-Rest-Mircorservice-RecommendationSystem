5a10db223a990467b8223f1ad2a0e076f3d0ca52
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-03-12 12:24:10 +0200

    adding ApiResponse.java

---
+++ product-service/src/main/java/com/ecommerce/productservice/utility/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.ecommerce.productservice.utility;
+
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+public class ApiResponse {
+    private String message;
+    private Object data;
+    private OffsetDateTime time;
+
+    public ApiResponse(String message, Object data) {
+        this.message = message;
+        this.data = data;
+        this.time = java.time.OffsetDateTime.now();
+    }
+}
