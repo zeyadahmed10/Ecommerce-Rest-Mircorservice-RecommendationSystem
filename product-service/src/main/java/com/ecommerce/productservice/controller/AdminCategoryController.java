eeee9f79d8aa50a1a7ae10af58d14ccd0f6ad2f8
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2023-03-17 18:30:39 +0200

    add RoleConvertor and fix permitAll and Configuration not Working in product-service/WebSecurity.java

--- product-service/src/main/java/com/ecommerce/productservice/controller/AdminCategoryController.java
+++ product-service/src/main/java/com/ecommerce/productservice/controller/AdminCategoryController.java
@@ -8,16 +8,17 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/category")
+@PreAuthorize("hasAuthority('admin')")
 public class AdminCategoryController {
     @Autowired
     CategoryService categoryService;
     @Autowired
     HeaderGenerator headerGenerator;
-
     @PostMapping
     public ResponseEntity<?> addCategory(@Valid @RequestBody Category category, HttpServletRequest request) {
         Category savedCategory = categoryService.addCategory(category);
